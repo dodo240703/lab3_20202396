1a4afb9b1547f7cb68e45548df3f85b727ecc1f0
dodo240703 <a20202396@pucp.edu.pe>
2024-09-04 20:07:38 -0500

    gg

---
+++ lab2_20202396/src/main/java/com/example/lab2_20202396/Lab220202396Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20202396;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab220202396Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab220202396Application.class, args);
+	}
+
+}
---
+++ lab2_20202396/src/test/java/com/example/lab2_20202396/Lab220202396ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20202396;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220202396ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
